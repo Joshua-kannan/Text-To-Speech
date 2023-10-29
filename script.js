37be888c2caea982c681086508ddad6d601417a8
joshua-kannan <joshuakannanwork@gmail.com>
2023-10-29 12:41:56 +0530

    google api added

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let speech = new SpeechSynthesisUtterance();
-let voices = [];
-let voiceSelect = document.querySelector("select");
+const speech = new SpeechSynthesisUtterance();
+const voices = [];
+const voiceSelect = document.querySelector("select");
+const speakButton = document.querySelector("button");
+const downloadButton = document.getElementById("downloadAudio");
 
 // Function to speak the text
 const speakText = () => {
@@ -10,36 +12,63 @@ const speakText = () => {
 
 // Function to download the audio as an mp3 file
 const downloadAudio = () => {
-    // Create a new Blob containing the audio data
-    const blob = new Blob([speech.text], { type: 'audio/mpeg' });
+    const textToConvert = document.querySelector("textarea").value;
 
-    // Create an anchor element to trigger the download
-    const a = document.createElement('a');
-    a.href = URL.createObjectURL(blob);
+    // Make an API call to Google Text-to-Speech to generate the audio
+    fetch(`https://texttospeech.googleapis.com/v1/text:synthesize?key=AIzaSyCjSG8frFYXpDf_LmHlUWFqec1Zjq4yKyI`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            input: {
+                text: textToConvert,
+            },
+            voice: {
+                languageCode: 'en-US',
+                name: 'en-US-Wavenet-D',
+            },
+            audioConfig: {
+                audioEncoding: 'MP3',
+            },
+        }),
+    })
+    .then(response => response.arrayBuffer())
+    .then(audioData => {
+        const blob = new Blob([audioData], { type: 'audio/mp3' });
 
-    // Prompt the user for a file name
-    const fileName = prompt("Enter a file name for the audio:", "audio.mp3");
-    if (fileName !== null) {
-        a.download = fileName;
-        a.style.display = 'none';
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-    }
+        // Prompt the user for a file name
+        const fileName = prompt("Enter a file name for the audio:", "audio.mp3");
+        if (fileName !== null) {
+            const a = document.createElement('a');
+            a.href = URL.createObjectURL(blob);
+            a.download = fileName;
+            a.style.display = 'none';
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+    });
 };
 
 // Event listener for the Listen button
-document.querySelector("button").addEventListener("click", speakText);
+speakButton.addEventListener("click", speakText);
 
 // Event listener for the Download Audio button
-document.querySelector("#downloadAudio").addEventListener("click", downloadAudio);
+downloadButton.addEventListener("click", downloadAudio);
 
 // Event listener to populate voice options
 window.speechSynthesis.onvoiceschanged = () => {
-    voices = window.speechSynthesis.getVoices();
-    speech.voice = voices[0];
+    voices.length = 0; // Clear the existing voices
+    voices.push(...window.speechSynthesis.getVoices());
+    voiceSelect.innerHTML = ""; // Clear the select box options
 
-    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+    voices.forEach((voice, i) => {
+        const option = new Option(voice.name, i);
+        voiceSelect.add(option);
+    });
+
+    speech.voice = voices[0]; // Set the initial voice
 };
 
 voiceSelect.addEventListener("change", () => {
